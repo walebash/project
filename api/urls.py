b7ff4ed25c9ab73a4b878288a34e7993032bb9b4
unknown <bashiradigun78@gmail.com>
2021-08-02 22:17:15 +0100

    app completed

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet
+
+
+router = SimpleRouter()
+router.register('posts', PostViewSet)
+
+urlpatterns = router.urls
---
+++ users/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm
+    return render(request, 'home/register.html', {'form': form})
+@login_required
+def profile(request):
+    form = PostForm(request.POST)
+    if form.is_valid():
+        form.save()
+        form = PostForm()
+    return render(request, 'home/profile.html', {'form':form})
+    
